ca8d90a3837b17d15f9865c1603288a0fe00087a
Your Name <you@example.com>
2015-11-01 12:47:09 +0000

    If the target_page is a :record then a block can now be passed to the update() method to modify the Kvx object before it is publish to HTML.

--- lib/daily_notices.rb
+++ lib/daily_notices.rb
@@ -12,29 +12,17 @@ class DailyNotices
   attr_accessor :title, :description, :link, :dx_xslt, :rss_xslt
 
   def initialize(filepath='', url_base: 'http:/127.0.0.1/', \
-                                            dx_xslt: '', rss_xslt: '')
+           dx_xslt: '', rss_xslt: '', target_page: :recordset, target_xslt: '')
     
-    @filepath, @url_base, @dx_xslt, @rss_xslt = filepath, \
-                                                    url_base, dx_xslt, rss_xslt
-        
-    @archive_path = Time.now.strftime("%Y/%b/%d").downcase
+    @filepath, @url_base, @dx_xslt, @rss_xslt, @target_page, @target_xslt = \
+                filepath, url_base, dx_xslt, rss_xslt, target_page, target_xslt
+
     
-    # If the file doesn't already exist in the 
-    #                    archive directory then symlink it
-    @indexpath = File.join(@filepath, @archive_path, 'index.xml')
-    FileUtils.mkdir_p File.dirname(@indexpath)
+    @schema ||= 'items/item(description, time)'
+    @default_key ||= 'uid'
     
-    @schema = 'items/item(description, time)'
-    @default_key = 'uid'
-        
-    if File.exists? @indexpath then
-      @dx = Dynarex.new @indexpath
-    else
-      @dx = Dynarex.new @schema
-      @dx.order = 'descending'
-      @dx.default_key = @default_key
-      @dx.xslt = @dx_xslt
-    end
+    @day = Time.now.day
+    new_day()
     
     # open the Dynarex file or create a new Dynarex file
 
@@ -50,23 +38,56 @@ class DailyNotices
       @rss.link = @url_base
     end    
     
+    # :recordset or : record
+    @target_page = target_page
   end
   
-  def create(description, time=Time.now, title: nil, \
-                                id: Time.now.strftime('%H%M%S'))
+  def create(x, time=Time.now, title: nil, \
+                     id: Time.now.strftime('%H%M%S'), description: nil)
 
-    @dx.create({description: description, time: time}, id: id)
+    new_day() if @day != Time.now.day
+        
+    if x.is_a? String then
+
+      description = x
+      @dx.create({description: description, time: time}, id: id)
+
+    elsif x.is_a? Hash
+
+      @dx.create(x, id: id)
+      
+    end
+    
     @dx.save @indexpath
-    File.write  File.join(@filepath, @archive_path, 'index.html'), \
-                                                  @dx.to_html(domain: @url_base)
+    
+    if @target_page == :recordset then
+      File.write  File.join(@filepath, @archive_path, 'index.html'), \
+                                                    @dx.to_html(domain: @url_base)
+    else
+
+      target_path = File.join(@filepath, @archive_path, id, 'index.html')
+      FileUtils.mkdir_p File.dirname(target_path)
+      rx = @dx.find(id)
+      
+      kvx = rx.to_kvx
+      yield kvx if block_given? 
+      
+      rxdoc = Rexle.new(kvx.to_xml)
+      rxdoc.instructions  << ['xml-stylsheet',\
+          "title='XSL_formatting' type='text/xsl' href='#{@target_xslt}'"]
+      
+      File.write target_path.sub(/\.html$/,'.xml', ), rxdoc.xml(pretty: true)
+      File.write  target_path, rx.to_html(xslt: @target_xslt)
+    end
     
     # Add it to the RSS document
     title ||= description.split(/\n/,2).first[0..140]
     link = [File.join(@url_base, File.basename(@filepath), \
-                                            @archive_path, '#' + id)].join('/')
+                                            @archive_path, id)].join('/')
     @rss.add( {title: title, link: link, description: description}, id: id)
     @rss.save @rssfile
-
+    
+    on_add(@indexpath, id)
 
   end
   
@@ -94,6 +115,38 @@ class DailyNotices
   
   def description=(val)
     @rss.description = val
+  end
+  
+  # If you wish override this method or use it in block form to add a  
+  #  notifier, callback routine or webhook, whenever a new record is added.
+  #
+  def on_add(xmlpath, id)
+    
+    yield(xmlpath, id) if block_given?
+    
+  end
+  
+  private 
+  
+  # configures the target page (using a Dynarex document) for a new day
+  #
+  def new_day()
+    
+    @archive_path = Time.now.strftime("%Y/%b/%d").downcase
+    
+    @indexpath = File.join(@filepath, @archive_path, 'index.xml')
+    FileUtils.mkdir_p File.dirname(@indexpath)
+    
+        
+    if File.exists? @indexpath then
+      @dx = Dynarex.new @indexpath
+    else
+      @dx = Dynarex.new @schema
+      @dx.order = 'descending'
+      @dx.default_key = @default_key
+      @dx.xslt = @dx_xslt
+    end    
+    
   end
   
 end
